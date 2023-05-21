41d2d71f32c088f531daea6c3cb6fe05da8cfc2b
wangkui <wangkui@asiainfo.com>
2023-05-21 17:06:42 +0800

    添加前端登录页面

--- jenkinsvue/src/App.vue
+++ jenkinsvue/src/App.vue
@@ -1,23 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
+    <br><br>
+    密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
+    <br><br>
+    <button v-on:click="login">登录</button>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'App'
+  name: 'Login',
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      responseResult: []
+    }
+  },
+  methods: {
+    login () {
+      this.$axios
+        .post('/login', {
+          username: this.loginForm.username,
+          password: this.loginForm.password
+        })
+        .then(successResponse => {
+          if (successResponse.data.code === 200) {
+            this.$router.replace({path: '/index'})
+          }
+        })
+        .catch(failResponse => {
+        })
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
